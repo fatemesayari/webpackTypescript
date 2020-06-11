08ebcbb6261c61c9333f28fbbc650385a86b8a33
fatemesayari <fateme.sayari@gmail.com>
2020-06-11 14:58:24 +0430

    first commit

---
+++ src/js/app.ts
@@ -0,0 +1,36 @@
+import '../css/main.css';
+import '../css/input-elements.css';
+import * as _ from "lodash";
+
+import {secretButton, secretParagraph } from './dom-loader';
+
+let showSecret: boolean = false;
+
+secretButton!.addEventListener('click', toggleSecretState);
+
+updateSecretParagraph();
+
+//methods
+function toggleSecretState(): void
+{
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton();
+}
+
+function updateSecretButton(): void
+{
+    if (showSecret)
+        secretButton!.textContent = 'Hide the Secret';
+    else
+        secretButton!.textContent = 'Show the Secret';
+}
+
+function updateSecretParagraph(): void
+{
+    if (showSecret)
+        (<HTMLElement>secretParagraph!).style.display = 'block';
+    else
+        (<HTMLElement>secretParagraph!).style.display = 'none';
+}
+console.log('lodash version:', _.VERSION);
